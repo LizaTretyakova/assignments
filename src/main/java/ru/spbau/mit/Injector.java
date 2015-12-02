3e8818269640f291509061a1cef08d4e103dc61a
LizaTretyakova <elizabet.tretyakova@gmail.com>
2015-12-03 02:28:13 +0300

    CW02, Лиза Третьяковаб подгруппа 1

--- src/main/java/ru/spbau/mit/Injector.java
+++ src/main/java/ru/spbau/mit/Injector.java
@@ -1,15 +1,64 @@
 package ru.spbau.mit;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 
 public class Injector {
+    private static HashMap<String, Boolean> used = new HashMap<>();
+    private static HashMap<String, Object> instances = new HashMap<>();
+    private static List<String> implClassNames;
 
-    /**
-     * Create and initialize object of `rootClassName` class using classes from
-     * `implementationClassNames` for concrete dependencies.
-     */
     public static Object initialize(String rootClassName, List<String> implementationClassNames) throws Exception {
-        throw new IllegalStateException();
+        implClassNames = implementationClassNames;
+        Object res;
+        try {
+            res = createObjectByName(rootClassName);
+        } catch (InjectionCycleException | ImplementationNotFoundException | AmbiguousImplementationException e) {
+            throw e;
+        } catch (ClassNotFoundException e) {
+            throw new ImplementationNotFoundException();
+        } catch (Exception e) {
+            throw new RuntimeException();
+        }
+        return res;
+    }
+
+    private static Object createObjectByName(String resName) throws Exception {
+        //getting parameters' types we need to create an instance of resName
+        List<Class<?>> paramTypes;
+        paramTypes = Arrays.asList(Class.forName(resName).getConstructors()[0].getParameterTypes());
+
+        ArrayList<Object> params = new ArrayList<>();
+        for (Class<?> paramType : paramTypes) {
+            String parameterTypeName = paramType.getName();//next parameter type we need
+            if (used.containsKey(parameterTypeName)) {//if we used it that's a cycle
+                throw new InjectionCycleException();
+            }
+            used.put(parameterTypeName, true);
+
+            int cnt = 0;
+            for (String implName : implClassNames) {//search for implementation of the current parameter type
+                if (Class.forName(parameterTypeName).isAssignableFrom(Class.forName(implName))) {
+                    if (!instances.containsKey(parameterTypeName)) {
+                        if (cnt > 0) {
+                            throw new AmbiguousImplementationException();
+                        }
+                        cnt++;
+                        instances.put(parameterTypeName, createObjectByName(implName));
+                    }
+                }
+            }
+            if (!instances.containsKey(parameterTypeName)) {
+                throw new ImplementationNotFoundException();
+            }
+            params.add(instances.get(parameterTypeName));
+            used.put(parameterTypeName, false);
+        }
+
+
+        return Class.forName(resName).getConstructors()[0].newInstance(params.toArray());
     }
 }
